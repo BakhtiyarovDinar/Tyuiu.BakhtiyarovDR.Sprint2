cda4f1f7b0949e8f5df9f638afa6f0fe5ab45d31
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-10-05 22:15:10 +0500

    Task1

---
+++ Tyuiu.BakhtiyarovDR.Sprint2.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+namespace Tyuiu.BakhtiyarovDR.Sprint2.Task1.V15.Lib
+{
+    public class DataService : ISprint2Task1V15
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool res1, res2, res3, res4, res5, res6;
+
+            res1 = (a + 202 > b) | (c - 8 < d);   
+            res2 = (a + 2 > b) & (c < d);    
+            res3 = (a > b) || (c < d);    
+            res4 = (a + 2 > b) && (c < d);   
+            res5 = !(res1);   
+            res6 = (b > a) ^ (c < d);  
+
+            bool[] array = new bool[] { res1, res2, res3, res4, res5, res6 };
+
+            return array;
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint2.Task1.V15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BakhtiyarovDR.Sprint2.Task1.V15.Lib;
+
+// a=415
+// b=616
+// c=134
+// d=127
+
+namespace Tyuiu.BakhtiyarovDR.Sprint2.Task1.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a, b, c, d;
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите d: ");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            bool[] array = new bool[] { };
+            array = ds.GetLogicOperations(a, b, c, d);
+
+            foreach (bool i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
